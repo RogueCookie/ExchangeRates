a81d52a8382dad17be6585844844a1abfc3084f7
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2021-02-04 10:01:57 +0200

    SetUpSwagger for Report API

---
+++ Services/ReportApi/Controllers/ReportController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ReportApi.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [Produces("application/json", "application/xml")]
+    [ApiController]
+    public class ReportController : Controller
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public ActionResult<string> Test()
+        {
+            return "Hello, World!";
+        }
+    }
+}
--- Services/ReportApi/Startup.cs
+++ Services/ReportApi/Startup.cs
@@ -5,8 +5,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ReportApi
 {
@@ -16,11 +22,60 @@ namespace ReportApi
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                services.AddSwaggerGen(c =>
+                {
+                    c.SwaggerDoc("v1", new OpenApiInfo
+                    {
+                        Title = "Exchange report api",
+                        Description = "API for work with microservices architecture, part of Report api",
+                        Version = "v1",
+                        Contact = new OpenApiContact()
+                        {
+                            Name = "Valeriia Vaganova",
+                            Email = "v9527906422@gmail.com"
+                        }
+                    });
+
+                    // path from which project to read xml documentation
+                    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+                    
+                    c.IncludeXmlComments(xmlCommentsFullPath);
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint
+            app.UseSwagger();
+
+            // Specify the Swagger endpoint
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"v1/swagger.json", $"Exchange rates report v1");
+                c.DisplayRequestDuration();
+            });
+
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    context.Response.ContentType = "text/html";
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    var exceptionHandlerFeature =
+                        context.Features.Get<IExceptionHandlerFeature>();
+                    logger.LogError(new EventId(), exceptionHandlerFeature.Error, exceptionHandlerFeature.Error.Message);
+
+                    await context.Response.WriteAsync("Something wrong");
+                });
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,10 +85,7 @@ namespace ReportApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
