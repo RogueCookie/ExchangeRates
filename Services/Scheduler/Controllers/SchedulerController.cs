a54e8ba0369000c8636c868798472d86529db88c
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2021-02-04 11:30:47 +0200

    SetUp scheduler api

---
+++ Services/Scheduler/Controllers/SchedulerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Scheduler.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [Produces("application/json", "application/xml")]
+    [ApiController]
+    public class SchedulerController : Controller
+    {
+        /// <summary>
+        /// test comments
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public ActionResult<string> Test()
+        {
+            return "Hello, Scheduler!";
+        }
+    }
+}
