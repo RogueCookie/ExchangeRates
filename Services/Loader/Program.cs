ce18539089de47061d46c9be832761f6b71373cc
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2021-02-07 19:56:06 +0200

    Ptrpared based configuratino for console app

--- Services/Loader/Program.cs
+++ Services/Loader/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OzExchangeRates.Core;
+using Serilog;
 
 namespace Loader
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static IConfiguration _configuration;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var host = new ServiceHost(args);
+
+           /* host.ConfigureServices((builderContext, services) =>
+            {
+                _configuration = builderContext.Configuration;
+
+                var connectionElasticLogger = _configuration.GetConnectionString("ElasticLogger");
+                services.AddSerilogToElasticLogging("ribbonUpdater", _configuration, connectionElasticLogger);
+
+                var connection = _configuration.GetConnectionString("Updater");
+                var workDb = _configuration.GetValue<string>("ConnectionString");
+                var schema = _configuration.GetValue<string>("MySchemaName");
+                services.Configure<RedisConfiguration>(_configuration.GetSection("Redis"));
+
+                services.AddDbContext<MyContext>(options => options
+                    .UseNpgsql(connection, x => x.MigrationsHistoryTable("_migrations_history", schema))
+                    .UseSnakeCaseNamingConvention());
+
+                services.AddDbContext<EventStorageContext>(options => options
+                    .UseNpgsql(workDb)
+                    .UseSnakeCaseNamingConvention());
+
+                services.AddSingleton<IRpcClient, RpcClient>();
+                services.AddSingleton<DbInitializator>();
+            },
+                (services) =>
+                {
+                    return new AutofacServiceProviderFactory((container) =>
+                    {
+                        container.Populate(services);
+                    });
+                });*/
+            await host.RunAsync((serviceProvider) =>
+            {
+                var baseInit = _configuration.GetValue<bool?>("BaseInit");
+                Log.Information($"Base Init - {baseInit}");
+                if (baseInit.HasValue && baseInit.Value)
+                {
+                    try
+                    {
+                        //var dbInit = serviceProvider.GetRequiredService<DbInitializator>();
+                        //dbInit.Initialize().GetAwaiter().GetResult();
+                    }
+                    catch (Exception exception)
+                    {
+                        Log.Error(exception.Message);
+                    }
+                }
+                //var eventBus = serviceProvider.GetRequiredService<RabbitMQClient>();
+                //eventBus.Start();
+            });
         }
     }
 }
+
