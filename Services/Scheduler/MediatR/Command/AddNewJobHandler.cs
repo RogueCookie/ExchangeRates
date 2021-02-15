761e9c7d4a0957103d3481af15f2c4d284c9ef53
Valeriia Vaganova <valeriia.vaganova@ru.abb.com>
2021-02-15 20:06:03 +0200

    add job by mediator

---
+++ Services/Scheduler/MediatR/Command/AddNewJobHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Scheduler.MediatR.Command
+{
+    public class AddNewJob : IRequest
+    {
+
+    }
+
+    public class AddNewJobHandler : IRequestHandler<AddNewJob>
+    {
+        public Task<Unit> Handle(AddNewJob request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
